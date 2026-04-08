dafc39c1cba6db352cbc703928ea0d70a54acb67
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-07 20:54:13 -0500

    feat(taller0): ejercicio 43 Explicacion de la teoria plano vs instancia

--- src/taller0/Computador.java
+++ src/taller0/Computador.java
@@ -11,4 +11,14 @@ public class Computador {
         System.out.println("RAM: " + ram);
         System.out.println("Precio: " +"$"+precio);
     }
+    
+    /*La diferencia radica en la existencia física y la asignación de memoria. 
+    El plano conceptual (la clase Computador) 
+    es solo una definición lógica escrita en código; 
+    no ocupa espacio en la memoria RAM del sistema 
+    ni puede realizar tareas, ya que es solo una idea de 
+    cómo debe ser un computador. Por otro lado, una máquina concreta 
+    (el objeto o instancia) es la materialización de ese plano. Cuando usamos new Computador(),
+    reservamos un espacio real en la memoria donde los atributos dejan de ser etiquetas y pasan a tener
+    valores reales, permitiendo que el programa interactúe con ella*/
 }
