c14d265907de4ee7f60b5b92f39eb5ec52a79715
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 18:48:15 -0500

    feat(taller0): ejercicio 56 - clase Temperatura con metodo para la conversion de Celcius a Fahrenheit

---
+++ src/taller0/Temperatura.java
@@ -0,0 +1,11 @@
+package taller0;
+
+public class Temperatura {
+    double celsius;
+
+    // Método para convertir a Fahrenheit
+    public double obtenerFahrenheit() {
+        return (celsius * 9 / 5) + 32;
+    }
+
+}
