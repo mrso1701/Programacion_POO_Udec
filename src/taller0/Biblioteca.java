7a680868f19ccec61fbba746a4f7ee630a112738
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:26:08 -0500

    docs: análisis y estructura de clase Biblioteca (Ejercicio 62)

---
+++ src/taller0/Biblioteca.java
@@ -0,0 +1,16 @@
+package taller0;
+/*
+ * EJERCICIO 62
+ * Análisis de Clase: Biblioteca
+ * Atributos: nombre, direccion, cantidadLibros
+ * Métodos: prestarLibro(), recibirLibro()
+ * Objetos reales: Biblioteca Bartolome Calvo, Biblioteca de la Universidad de Cartagena.
+ */
+public class Biblioteca {
+    String nombre;
+    String direccion;
+    int cantidadLibros;
+
+    public void prestarLibro() {}
+    public void recibirLibro() {}
+}
