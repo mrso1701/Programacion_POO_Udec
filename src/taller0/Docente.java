6554e1fea76968d22bbcbfb6dce14d4144985916
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:23:27 -0500

    docs: análisis y estructura de clase Docente (Ejercicio 61)

---
+++ src/taller0/Docente.java
@@ -0,0 +1,18 @@
+
+package taller0;
+/*
+ * EJERCICIO 61
+ * Análisis:
+ * Atributos: nombre, especialidad, añosExperiencia
+ * Métodos: calificar(), dictarClase()
+ * Objetos reales: El profesor John Arrieta, un profesor de matemáticas.
+ */
+public class Docente {
+    String nombre;
+    String especialidad;
+    int añosExperiencia;
+    
+    public void calificar(){}
+    public void dictarClase(){}
+    
+}
