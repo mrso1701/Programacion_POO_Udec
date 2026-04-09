cad6605a4fd793476c74b9cc6565219060481308
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:07:11 -0500

    feat(taller0): agregar clase Avion con método abordar

---
+++ src/taller0/Avion.java
@@ -0,0 +1,12 @@
+package taller0;
+
+public class Avion {
+    String aerolinea;
+    String destino;
+    int pasajeros;
+
+    // Método para abordar pasajeros
+    public void abordar(int cantidad) {
+        pasajeros += cantidad;
+    }
+}
