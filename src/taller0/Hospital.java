c0c2bbfd53af01dace9c51216516ecbf4bcfdc77
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:27:28 -0500

    docs: análisis y estructura de clase Hospital (Ejercicio 63)

---
+++ src/taller0/Hospital.java
@@ -0,0 +1,17 @@
+
+package taller0;
+/*
+ * EJERCICIO 63
+ * Análisis de Clase: Hospital
+ * Atributos: nombre, capacidadCamas, especialidades
+ * Métodos: ingresarPaciente(), darAlta()
+ * Objetos reales: Hospital Universitario del Caribe, Clínica Medihelp.
+ */
+public class Hospital {
+    String nombre;
+    int capacidadCamas;
+    String especialidades;
+
+    public void ingresarPaciente() {}
+    public void darAlta() {}
+}
