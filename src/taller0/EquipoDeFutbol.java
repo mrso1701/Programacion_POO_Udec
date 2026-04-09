50a921ff7adfdd97f0d7684b8552f8fa73d95f75
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:30:57 -0500

    docs: análisis y estructura de clase EquipoDeFutbol (Ejercicio 65)

---
+++ src/taller0/EquipoDeFutbol.java
@@ -0,0 +1,16 @@
+package taller0;
+/*
+ * EJERCICIO 65
+ * Análisis de Clase: EquipoDeFutbol
+ * Atributos: nombreEquipo, estadio, puntosEnLiga
+ * Métodos: jugarPartido(), entrenar()
+ * Objetos reales: Real Cartagena, Junior de Barranquilla.
+ */
+public class EquipoDeFutbol {
+    String nombreEquipo;
+    String estadio;
+    int puntosEnLiga;
+
+    public void jugarPartido() {}
+    public void entrenar() {}
+}
