c711627c245cf8938c84ff21d29d8bbdb2d0caab
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:28:57 -0500

    docs: análisis y estructura de clase Tienda (Ejercicio 64)

---
+++ src/taller0/Tienda.java
@@ -0,0 +1,16 @@
+package taller0;
+/*
+ * EJERCICIO 64
+ * Análisis de Clase: Tienda
+ * Atributos: nombreTienda, tipoProducto, saldoCaja
+ * Métodos: vender(), abastecerInventario()
+ * Objetos reales: Tienda "La Esquina", Supermercado local.
+ */
+public class Tienda {
+    String nombreTienda;
+    String tipoProducto;
+    double saldoCaja;
+
+    public void vender() {}
+    public void abastecerInventario() {}
+}
