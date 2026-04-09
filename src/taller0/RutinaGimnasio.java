bd44b1375b578ebacc9d700b3a23ef0cdc10c675
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-08 19:44:10 -0500

    feat(taller0): ejercicio 67 - creacion de clase RutinaGimnasio con metodo completarSerie

---
+++ src/taller0/RutinaGimnasio.java
@@ -0,0 +1,11 @@
+package taller0;
+
+public class RutinaGimnasio {
+    String musculoEntrenado;
+    int series;
+    int repeticiones;
+
+    public void completarSerie() {
+        System.out.println("Serie completada de " + musculoEntrenado);
+    }
+}
