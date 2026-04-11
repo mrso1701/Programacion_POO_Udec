b14ffeb43c79b947e73cac7cb3ce115cc95b2daa
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-11 17:03:09 -0500

    Taller 4: Ejercicio 1 - Clase Estudiante con atributos privados y encapsulamiento

--- src/taller4/Estudiante.java
+++ src/taller4/Estudiante.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taller4;
 
-/**
- *
- * @author LENOVO
- */
+
 public class Estudiante {
-    
+    private String nombre;
+    private int edad;
+    private double notaPromedio;
+
+    public Estudiante(String nombre, int edad, double notaPromedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.notaPromedio = notaPromedio;
+    }
+
+    // Metodos get y set con validaciones 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad >= 0) { // Validacion simple
+            this.edad = edad;
+        }
+    }
+
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+
+    public void setNotaPromedio(double notaPromedio) {
+        if (notaPromedio >= 0.0 && notaPromedio <= 5.0) { // Validacion de rango
+            this.notaPromedio = notaPromedio;
+        }
+    }
 }
--- src/taller4/Main.java
+++ src/taller4/Main.java
@@ -3,7 +3,14 @@ package taller4;
 public class Main {
 
     public static void main(String[] args) {
+        Estudiante est = new Estudiante("Andres", 18, 4.5);
+
+        // Acceso a traves de metodos get
+        System.out.println("Nombre: " + est.getNombre());
         
+        // Modificacion a traves de metodos set
+        est.setNotaPromedio(4.8);
+        System.out.println("Nueva Nota: " + est.getNotaPromedio());
     }
     
 }
