79728e4ddb8681776c9dac3cb6df8a638e57f896
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-11 15:36:37 -0500

    Taller 2: Ejercicio 3 - Corrección de error de this en contexto estático

--- src/taller2/EjemploError.java
+++ src/taller2/EjemploError.java
@@ -1,10 +1,19 @@
 package taller2;
 
 public class EjemploError {
-    private String mensaje = "Hola";
+    private String detalle;
 
-    public static void metodoEstatico() {
-        // ERROR: No se puede usar this en un contexto estatico
-        System.out.println(this.mensaje);
+    public EjemploError(String detalle) {
+        this.detalle = detalle;
+    }
+
+    // No usamos "this" aqui porque los metodos estaticos no conocen al objeto actual
+    public static void informacionEstatica() {
+        System.out.println("Este es un metodo estatico. No puede usar 'this'.");
+    }
+
+    // Metodo de instancia (Uso correcto de this)
+    public void mostrarDetalle() {
+        System.out.println("Detalle: " + this.detalle);
     }
 }
