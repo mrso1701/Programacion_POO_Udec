12f823b141af251d9ba3b106c0e6dbe6851ac792
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-11 18:29:39 -0500

    Taller3: correciones en el codigo

--- src/taller3/Persona.java
+++ src/taller3/Persona.java
@@ -3,14 +3,14 @@ package taller3;
 public class Persona {
     private String nombre; // Atributo de instancia (no estatico)
 
-    // 1. Metodo static que intenta acceder a un atributo no estatico
+    // Metodo static que intenta acceder a un atributo no estatico
     public static void metodoConError() {
         // System.out.println(this.nombre); 
         // ERROR DE COMPILACIÓN: No se puede usar 'this' ni atributos de instancia
         // en un contexto estático porque 'static' pertenece a la clase y no al objeto[cite: 375, 379].
     }
 
-    // 2. Código corregido
+    // Código corregido
     public void metodoCorrecto() {
         System.out.println("Nombre: " + this.nombre); // Aquí sí funciona porque es un método de instancia[cite: 404].
     }
