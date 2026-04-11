696d6b759ee8f3bad44a9332edd869dcb1a02fac
mrso1701 <andresrodriguez172008@gmail.com>
2026-04-11 15:03:03 -0500

    feat(Taller1): Ejercicio 2 - Clase CuentaBancaria con sobrecarga de constructores

---
+++ src/taller1/CuentaBancaria.java
@@ -0,0 +1,36 @@
+package taller1;
+
+public class CuentaBancaria {
+    String numeroCuenta;
+    double saldo;
+    String tipoCuenta;
+
+    //Constructor por defecto [cite: 125]
+    public CuentaBancaria() {
+        this.numeroCuenta = "0000000000";
+        this.saldo = 0.0;
+        this.tipoCuenta = "No definido";
+    }
+
+    //Constructor parametrizado con dos parametros 
+    public CuentaBancaria(String numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        this.saldo = 0.0; // Saldo inicial por defecto
+    }
+
+    //Constructor sobrecargado con tres parametros [cite: 127]
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+
+    // Metodo para mostrar los detalles 
+    public void mostrarInformacion() {
+        System.out.println("Numero de Cuenta: " + numeroCuenta);
+        System.out.println("Saldo: $" + saldo);
+        System.out.println("Tipo de Cuenta: " + tipoCuenta);
+        System.out.println("---------------------------");
+    }
+}
